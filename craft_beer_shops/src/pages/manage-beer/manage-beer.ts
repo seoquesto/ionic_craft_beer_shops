85f4dc39d532cbf6da74495135f29641f98c8b6b
seoquesto <seoquesto@gmail.com>
2017-12-31 14:07:19 +0100

    Beer photo

--- craft_beer_shops/src/pages/manage-beer/manage-beer.ts
+++ craft_beer_shops/src/pages/manage-beer/manage-beer.ts
@@ -4,6 +4,7 @@ import { Beer } from './../../models/beer.model';
 import { ViewController } from 'ionic-angular/navigation/view-controller';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { ManagmentService } from '../../services/managment.service';
 
 @Component({
   selector: 'page-manage-beer',
@@ -14,9 +15,12 @@ export class ManageBeerPage implements OnInit {
   @ViewChild('f') form: NgForm;
   beer: Beer;
   mode: string = 'Edit';
+  photo: string = '';
+  showSpinner: boolean = false;
   
   constructor(private navParams: NavParams,
-              private viewController: ViewController) { }
+              private viewController: ViewController,
+              private managmentService: ManagmentService) { }
   
   ngOnInit(): void {
     this.beer = this.navParams.get('beer');
@@ -48,12 +52,33 @@ export class ManageBeerPage implements OnInit {
     if(this.beer.comments != null) {
       this.beer.comments.forEach(e=>comments.push(e));
     }
+
     return new Beer(this.form.form.get('name').value,
                     this.form.form.get('price').value,
-                    this.form.form.get('photo').value,
+                    this.photo,
                     this.form.form.get('percentages').value,
                     this.form.form.get('ibu').value,
                     this.form.form.get('plato').value,
                     comments);
+  }
+
+  async changePhotoFile(event: any) {
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+      let file: File = fileList[0];
+      this.presentSpinner();
+      await this.managmentService.setPhotoFile(file).then((data:string)=>{
+        this.photo = data;
+        this.hideSpinner();
+      });
+    }
+  }
+
+  private presentSpinner(){
+    this.showSpinner = true;
+  }
+
+  private hideSpinner() {
+    this.showSpinner = false;
   }
 }
