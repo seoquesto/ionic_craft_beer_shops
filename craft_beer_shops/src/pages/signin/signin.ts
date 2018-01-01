457872896184f806a68d49649d583fd35bf20dfc
seoquesto <seoquesto@gmail.com>
2018-01-01 17:17:37 +0100

    Story: Signin logic

--- craft_beer_shops/src/pages/signin/signin.ts
+++ craft_beer_shops/src/pages/signin/signin.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './../../services/auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Loading, AlertController, LoadingController, Alert } from 'ionic-angular';
 
 @Component({
   selector: 'page-signin',
@@ -7,7 +9,44 @@ import { Component } from '@angular/core';
 })
 
 export class SigninPage {
+  private loading: Loading;
+
+  constructor(private authService: AuthService, 
+    private alertController: AlertController,
+    private loadingController: LoadingController) { }
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.createLoadingTile();
+    this.authService.signin(form.form.get('email').value,
+                            form.form.get('email').value)
+                    .then((data)=> {
+                      this.hideLoadingTile();
+                    })
+                    .catch((error)=>{
+                      this.hideLoadingTile();
+                      this.backendAllert(error.message);
+                    });
+  }
+
+  private backendAllert(message: string): void {
+    if(message != null && message.trim().length > 1)
+    {
+      const alert: Alert = this.alertController.create({
+                                  title: 'Error',
+                                  message: message,
+                                  buttons: ['Ok']});
+      alert.present();
+    }
+  }
+
+  private createLoadingTile(): void {
+    this.loading = this.loadingController.create({
+      content: 'Signing you up...'
+    });
+    this.loading.present();
+  }
+
+  private hideLoadingTile(): void {
+    this.loading.dismiss();
   }
 }
--- craft_beer_shops/src/services/auth.service.ts
+++ craft_beer_shops/src/services/auth.service.ts
@@ -15,4 +15,8 @@ export class AuthService {
     signup(email: string, password: string) {
         return firebase.auth().createUserWithEmailAndPassword(email, password);
     }
+
+    signin(email: string, password: string) {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 }
