cdfab06ddd60d1fe76175317889d9661cfbc7235
seoquesto <seoquesto@gmail.com>
2018-01-01 17:12:32 +0100

    Story: Signup logic

--- craft_beer_shops/src/pages/signup/signup.ts
+++ craft_beer_shops/src/pages/signup/signup.ts
@@ -1,5 +1,10 @@
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { AuthService } from './../../services/auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { Loading } from 'ionic-angular';
+import { Alert } from 'ionic-angular/components/alert/alert';
 
 @Component({
   selector: 'page-signup',
@@ -7,7 +12,44 @@ import { Component } from '@angular/core';
 })
 
 export class SignupPage {
-  onSubmit(form: NgForm) {
-    console.log(form);
+  private loading: Loading;
+
+  constructor(private authService: AuthService, 
+              private alertController: AlertController,
+              private loadingController: LoadingController) { }
+
+  onSubmit(form: NgForm): void {
+    this.createLoadingTile();
+    this.authService.signup(form.form.get('email').value, 
+                            form.form.get('password').value)
+                            .then((result)=>{
+                              this.hideLoadingTile();
+                            })
+                            .catch((error)=>{
+                              this.hideLoadingTile();
+                              this.backendAllert(error.message);
+                            });
+  }
+
+  private backendAllert(message: string): void {
+    if(message != null && message.trim().length > 1)
+    {
+      const alert: Alert = this.alertController.create({
+                                  title: 'Error',
+                                  message: message,
+                                  buttons: ['Ok']});
+      alert.present();
+    }
+  }
+
+  private createLoadingTile(): void {
+    this.loading = this.loadingController.create({
+      content: 'Signing you up...'
+    });
+    this.loading.present();
+  }
+
+  private hideLoadingTile(): void {
+    this.loading.dismiss();
   }
 }
--- craft_beer_shops/src/services/auth.service.ts
+++ craft_beer_shops/src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { User } from './../models/user.model';
+import * as firebase from 'firebase';
 
 export class AuthService {
     private user = new User('Adam','https://www.shareicon.net/data/128x128/2015/09/18/103160_man_512x512.png');
@@ -9,5 +10,9 @@ export class AuthService {
     
     getUser() {
         return this.user;
+    }
+
+    signup(email: string, password: string) {
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
     }
 }
