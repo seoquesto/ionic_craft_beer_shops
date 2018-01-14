5786bc33c6d18feca35e81173356926b73574ceb
seoquesto <seoquesto@gmail.com>
2018-01-14 22:59:42 +0100

    Bug: Location managment should require GPS

--- craft_beer_shops/src/pages/set-location/set-location.ts
+++ craft_beer_shops/src/pages/set-location/set-location.ts
@@ -2,6 +2,7 @@ import { ViewController } from 'ionic-angular/navigation/view-controller';
 import { NavParams } from 'ionic-angular';
 import { Location } from './../../models/location.model';
 import { Component, OnInit } from '@angular/core';
+import { LoadingService } from '../../services/loading.service';
 
 @Component({
   selector: 'page-set-location',
@@ -14,7 +15,8 @@ export class SetLocationPage implements OnInit {
   hasAlreadyLocation: boolean = false;
   
   constructor(private navParams: NavParams,
-              private viewController: ViewController) { }
+              private viewController: ViewController,
+              private loadingService: LoadingService) { }
 
   ngOnInit(): void {
     this.location = new Location(this.navParams.get('lat'), this.navParams.get('lng'));
@@ -24,9 +26,17 @@ export class SetLocationPage implements OnInit {
       this.markerLocation = new Location(this.location.lat, this.location.lng);
     } else {
       if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((pos)=>{
-          this.location = new Location(pos.coords.latitude, pos.coords.longitude);
-        });
+        navigator.geolocation.getCurrentPosition(
+          (pos)=>{
+            this.location = new Location(pos.coords.latitude, pos.coords.longitude);},
+          (error)=>{
+            this.loadingService.createLoadingTile("Please enable your GPS!");
+            setTimeout(()=>{
+              this.loadingService.hideLoadingTile();
+              this.onAbort();
+            },3000);
+          }
+          );
       }
     }
   }
